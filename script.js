8b22a70541263d5554c1ecc85923ae34ab34aa07
harshita604 <harshitastanwar@gmail.com>
2025-01-10 01:28:29 +0530

    Added .env file

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const apikey="449ea6a7271020ea442ad2efdcf80965";
-const apiurl=" https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+require('dotenv').config();
+const apiKey = process.env.API_KEY;
+const apiUrl = process.env.API_URL;
+
+console.log(`API Key: ${apiKey}`);
+console.log(`API URL: ${apiUrl}`);
+
 const searchBox= document.querySelector(".search input");
 const searchBtn= document.querySelector(".search button");
 const weathericon= document.querySelector(".weather-icon");
